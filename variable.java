0c0139ecbaace2cbe369a07113ab308f6c0e45f1
abhishektisage <163665372+abhishektisage@users.noreply.github.com>
2025-01-03 23:25:09 +0530

    Add files via upload

---
+++ variable.java
@@ -0,0 +1,14 @@
+public class variable{
+    public static void main(String args[]){
+        int a =111;
+        int b=220;
+        String name="ABHI";
+        int age =20;
+        char gen='M';
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(gen);
+    }
+}
