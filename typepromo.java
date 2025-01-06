c05b36d0c45c98460a6e75d8cb156c6c39308b6b
abhishektisage <163665372+abhishektisage@users.noreply.github.com>
2025-01-06 23:10:14 +0530

    Add files via upload

---
+++ typepromo.java
@@ -0,0 +1,10 @@
+public class typepromo{
+    public static void main(String args[]){
+        int a = 25;
+        char b='a';
+        byte c= 5;
+        short d =10;
+        System.out.println(a+b+c+d);//ans will get in int
+    }
+
+}
