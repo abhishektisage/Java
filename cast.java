f97a26f525a0880938f9f4ffdf1e456708665f0c
abhishektisage <163665372+abhishektisage@users.noreply.github.com>
2025-01-06 23:09:55 +0530

    Add files via upload

---
+++ cast.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class cast{
+    public static void main(String args[]){
+      Scanner sc= new Scanner(System.in);
+      float a=sc.nextFloat();
+      int b=(int)(a);
+      System.out.println(b);
+    }
+}
