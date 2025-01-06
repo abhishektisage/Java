b73fb2a02d340ffacb894ecf4a9835793e463972
abhishektisage <163665372+abhishektisage@users.noreply.github.com>
2025-01-06 23:10:36 +0530

    Add files via upload

---
+++ typepromo1.java
@@ -0,0 +1,9 @@
+public class typepromo1{
+    public static void main(String args[]){
+        long a=1000;
+        float b=1000.99f;
+        int c=1000;
+        double d=1000.00;
+        System.out.println(a+b+c+d);
+    }
+}
