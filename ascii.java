92fa6e3fe4fcfa6e0d3d0a4671f45f836aaa782e
abhishektisage <163665372+abhishektisage@users.noreply.github.com>
2025-01-06 23:09:31 +0530

    Add files via upload

---
+++ ascii.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class ascii{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int num=97;
+        int num1=98;
+        char ch= (char)num;
+        char ch1=(char)num1;
+        System.out.println(ch);
+        System.out.println(ch1);
+
+    }
+}
