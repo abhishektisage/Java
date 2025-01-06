e68a7f6f8a037aa35f5ee0925261418036b1444d
abhishektisage <163665372+abhishektisage@users.noreply.github.com>
2025-01-06 23:09:08 +0530

    Add files via upload

---
+++ Area.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Area{
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+            float r=sc.nextFloat();
+            double pi=3.14;
+            double area=2*pi*r;
+            System.out.println(area);
+        
+    }
+}
