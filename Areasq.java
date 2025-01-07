820fb1ce8cf87a6672c5e5a43a70069133bf2f44
abhishektisage <163665372+abhishektisage@users.noreply.github.com>
2025-01-07 22:11:56 +0530

    Add files via upload

---
+++ Areasq.java
@@ -0,0 +1,10 @@
+
+import java.util.*;
+public class Areasq{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int s=sc.nextInt();
+        int area=s*s;
+        System.out.println(area);
+    }
+}
