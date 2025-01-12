f7c5a83598ec5328c762a4d7eea712f1476c9c13
abhishektisage <163665372+abhishektisage@users.noreply.github.com>
2025-01-12 22:29:19 +0530

    Add files via upload

---
+++ Week.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Week{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int day=sc.nextInt();
+        
+        switch(day){
+            case 1: System.out.println("MONDAY");
+            break;
+            case 2: System.out.println("TUESDAY");
+            break;
+            case 3: System.out.println("WEDNESDAY");
+            break;
+            case 4: System.out.println("THRUSDAY");
+            break;
+            case 5: System.out.println("FRIDAY");
+            break;
+            case 6: System.out.println("SATURDAY");
+            break;
+            case 7: System.out.println("SUNDAY");
+            break;
+
+            default:System.out.println("there are only 7 weeks");
+
+        }
+    }
+}
