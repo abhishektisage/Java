fb951e32c0e59801428e9c4fa412f7d5dc0941a7
abhishektisage <163665372+abhishektisage@users.noreply.github.com>
2025-01-12 22:30:19 +0530

    Add files via upload

---
+++ switchex.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class switchex{
+ public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    System.out.print("enter your choice:");
+    int num=sc.nextInt();
+    switch(num){
+        case 1: System.out.println("hieeeee");
+                break;
+        case 2: System.out.println("my name is abhishek");
+                break;
+        case 3: System.out.println("studying  b.tech");
+                break;
+        default :System.out.println("wrong choice");
+
+    }
+
+ }
+
+
+}
