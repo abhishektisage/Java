37ae0a498c4cb6a63e02b4eaed24ad65b69fb2d4
abhishektisage <163665372+abhishektisage@users.noreply.github.com>
2025-01-12 22:32:18 +0530

    Add files via upload

---
+++ Pos_Neg.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Pos_Neg{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+
+        int num=sc.nextInt();
+
+        if(num>0){
+            System.out.println("the num is positive");
+        }
+        else if(num<0){
+            System.out.println("the num is negative");
+        }
+            
+        
+        else{
+            System.out.println("it is a zero");
+        }
+
+    }
+    
+}
