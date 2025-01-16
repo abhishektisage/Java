60700cd60eee66c2d56870c5e0964ab51aa7b7bf
abhishektisage <163665372+abhishektisage@users.noreply.github.com>
2025-01-16 23:28:24 +0530

    Add files via upload

---
+++ LOOPS/sqpattern.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class sqpattern{
+    public static void main(String args[]){
+
+        Scanner sc=new Scanner(System.in);
+        int line=sc.nextInt();
+        int i;
+
+        for(i=1 ; i<=line ; i++){
+
+            System.out.println("****");
+        }
+
+
+    }
+}
