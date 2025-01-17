f621f80c8bc8505cce42451bad391000b51298d8
abhishektisage <163665372+abhishektisage@users.noreply.github.com>
2025-01-17 22:31:01 +0530

    Add files via upload

---
+++ LOOPS/dohw.java
@@ -0,0 +1,12 @@
+public class dohw{
+    public static void main(String args[]){
+
+        int i=1;
+
+        do{
+            System.out.println("HELLO WORLD");
+            i++;
+
+        }while(i<=5);
+    }
+}
