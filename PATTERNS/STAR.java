afca4ed48bff3b4efff8a3109a2e238e8ce429a3
abhishektisage <163665372+abhishektisage@users.noreply.github.com>
2025-01-20 18:29:17 +0530

    Add files via upload

---
+++ PATTERNS/STAR.java
@@ -0,0 +1,16 @@
+public class STAR {
+    public static void main(String args[]){
+
+        int i;
+        int star;
+
+        for(i=1 ; i<=5 ; i++){
+
+            for(star=1; star<=i ; star++){
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
