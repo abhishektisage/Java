b34485164794dbeaf85a7802bf98773f2c49414f
abhishektisage <163665372+abhishektisage@users.noreply.github.com>
2025-01-20 18:29:48 +0530

    Add files via upload

---
+++ PATTERNS/HPyramid.java
@@ -0,0 +1,17 @@
+public class HPyramid{
+    public static void main(String args[]){ 
+
+        int i;
+        int number;
+
+        for(i=1 ; i<=4 ; i++){
+            for(number=1 ; number<=i ; number++){ 
+
+                System.out.print(number);
+
+            }
+            System.out.println();
+        }
+
+    }
+}
