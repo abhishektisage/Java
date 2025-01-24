5d02e3a62ac7e7f730b3f2564b09424bf23accc0
abhishektisage <163665372+abhishektisage@users.noreply.github.com>
2025-01-24 22:16:53 +0530

    Add files via upload

---
+++ Functions/helloW.java
@@ -0,0 +1,10 @@
+public class helloW{
+    public static void hello(){
+          System.out.println("Hello world");
+    }
+
+    public static void main(String args[]){
+        hello();
+        hello();
+    }
+}
