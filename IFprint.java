3efae17068b34e4c5672ff10fcc8dd89f99b4532
abhishektisage <163665372+abhishektisage@users.noreply.github.com>
2025-01-30 23:55:19 +0530

    Add files via upload

---
+++ IFprint.java
@@ -0,0 +1,20 @@
+public class IFprint {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        int number = 10;
+        double pi = 3.14;
+        String message = "Welcome to Java!";
+        
+        System.out.println("Number: " + number);
+        System.out.println("Value of Pi: " + pi);
+        System.out.println("Message: " + message);
+
+       
+        if (number > 5) {
+            System.out.println("Number is greater than 5");
+        } else {
+            System.out.println("Number is 5 or less");
+        }
+    }
+}
