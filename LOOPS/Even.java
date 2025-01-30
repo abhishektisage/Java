15dc78e7185429c4ac5dcde5aa43faece859dd7b
abhishektisage <163665372+abhishektisage@users.noreply.github.com>
2025-01-30 23:53:21 +0530

    Add files via upload

---
+++ LOOPS/Even.java
@@ -0,0 +1,12 @@
+public class Even {
+    public static void main(String[] args) {
+
+        
+        System.out.println("Even numbers from 2 to 20:");
+        for (int i = 2; i <= 20; i += 2) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+}
+    
