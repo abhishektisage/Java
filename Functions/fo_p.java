1b82cf5ddb0969eb69393731fcc33750f16dde8a
abhishektisage <163665372+abhishektisage@users.noreply.github.com>
2025-02-10 23:18:00 +0530

    Add files via upload

---
+++ Functions/fo_p.java
@@ -0,0 +1,14 @@
+public class fo_p{
+    public static int sum(int a,int b){
+        return a+b;
+
+    }
+    public static int sum(int a,int b,int c){
+        return a+b+c;
+    }
+
+    public static void main(String args[]){
+        System.out.println(sum(5,6));
+        System.out.println(sum(4,5,6));
+    }
+}
