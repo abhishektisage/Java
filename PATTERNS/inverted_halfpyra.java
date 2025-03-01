a6dd984f955af5fc573b5f0b7a5685fa6e268b4c
abhishektisage <163665372+abhishektisage@users.noreply.github.com>
2025-03-01 23:41:45 +0530

    Add files via upload

---
+++ PATTERNS/inverted_halfpyra.java
@@ -0,0 +1,13 @@
+public class inverted_halfpyra{
+    public static void invertedNum(int n){
+        for(int i=1 ; i<=n ; i++){
+            for(int j=1 ; j<=n-i+1 ; j++){
+                System.out.print(j +"");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String args[]){
+           invertedNum(6);
+    }
+}
