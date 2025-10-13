dd26c0ff4847854c544a56269fe58f9abfef78e8
abhishektisage <163665372+abhishektisage@users.noreply.github.com>
2025-10-13 21:02:27 +0530

    Add files via upload

---
+++ OOPS/Calc.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+class Calculator {
+
+    double add(double a, double b) {
+        return a + b;
+    }
+
+    double subtract(double a, double b) {
+        return a - b;
+    }
+
+    double multiply(double a, double b) {
+        return a * b;
+    }
+
+    double divide(double a, double b) {
+        if (b == 0) {
+            System.out.println("Cannot divide by zero!");
+            return 0;
+        } else {
+            return a / b;
+        }
+    }
+}
+
+public class Calc {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        Calculator calc = new Calculator(); // object creation
+
+        System.out.print("Enter first number: ");
+        double num1 = sc.nextDouble();
+
+        System.out.print("Enter second number: ");
+        double num2 = sc.nextDouble();
+
+        System.out.println("\n--- Operations ---");
+        System.out.println("Addition: " + calc.add(num1, num2));
+        System.out.println("Subtraction: " + calc.subtract(num1, num2));
+        System.out.println("Multiplication: " + calc.multiply(num1, num2));
+        System.out.println("Division: " + calc.divide(num1, num2));
+    }
+}
