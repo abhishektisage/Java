376c666bde1df5f5180711e914cf2b3bbe3f46e1
abhishektisage <163665372+abhishektisage@users.noreply.github.com>
2025-10-14 23:53:12 +0530

    Add files via upload

---
+++ RECURSION/sum.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class sum{
+    public static int calcSum(int n){
+        if(n==1){
+            return n;
+        }
+        int Snm1=calcSum(n-1);
+        int Sn=n+Snm1;
+        return Sn;
+
+    }
+    public static void main(String args[]){
+        int n=5;
+        System.out.println(calcSum(n));
+    }
+}
