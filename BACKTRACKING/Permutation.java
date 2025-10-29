639105d454fc22648aa5bd94b033f432b22b43f5
abhishektisage <163665372+abhishektisage@users.noreply.github.com>
2025-10-29 20:11:52 +0530

    Add files via upload

---
+++ BACKTRACKING/Permutation.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Permutation{
+    public static void findP(String str,String ans){
+
+       if(str.length()==0){
+        System.out.println(ans);
+        return;
+       }
+       for(int i=0;i<str.length();i++){
+        char curr=str.charAt(i);
+        String newStr=str.substring(0,i)+str.substring(i+1);
+
+        findP(newStr,ans+curr);
+       }
+
+    }
+    public static void main(String args[]){
+        String str="abc";
+        findP(str,"");
+    }
+}
